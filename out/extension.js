1c6075f7997ce533e114d221324d7cf4ca6ec3ee
JamisonHolt <JamisonHolt95@gmail.com>
2022-07-06 10:04:28 -0700

    test

---
+++ out/extension.js
@@ -0,0 +1,33 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.deactivate = exports.activate = void 0;
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+const vscode = require("vscode");
+const treeProvider_1 = require("./treeProvider");
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+function activate(context) {
+    const rootPath = vscode.workspace.workspaceFolders &&
+        vscode.workspace.workspaceFolders.length > 0
+        ? vscode.workspace.workspaceFolders[0].uri.fsPath
+        : undefined;
+    vscode.window.registerTreeDataProvider("nodeDependencies", new treeProvider_1.NodeDependenciesProvider(rootPath));
+    vscode.window.createTreeView("nodeDependencies", {
+        treeDataProvider: new treeProvider_1.NodeDependenciesProvider(rootPath),
+    });
+    // The command has been defined in the package.json file
+    // Now provide the implementation of the command with registerCommand
+    // The commandId parameter must match the command field in package.json
+    let disposable = vscode.commands.registerCommand("config-builder.helloWorld", () => {
+        // The code you place here will be executed every time your command is executed
+        // Display a message box to the user
+        vscode.window.showInformationMessage("Hello World!");
+    });
+    context.subscriptions.push(disposable);
+}
+exports.activate = activate;
+// this method is called when your extension is deactivated
+function deactivate() { }
+exports.deactivate = deactivate;
+//# sourceMappingURL=extension.js.map
